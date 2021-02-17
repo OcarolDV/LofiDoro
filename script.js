2e532d59d27b2cbf5766a8310368f0746a8e92cb
Dar Vince Ocarol <55413710+OcarolDV@users.noreply.github.com>
2021-02-16 20:18:16 -0800

    Uploaded Existing Files
    Responsive Design  and Alignment are to be fixed

---
+++ script.js
@@ -0,0 +1,53 @@
+let canvas = document.getElementById('canvas1');
+canvas.width = 800;
+canvas.height = innerHeight / 1.05;
+
+
+let ctx = canvas.getContext("2d");
+ctx.fillStyle = '#ed4242';
+ctx.strokeStyle = '#ed4242';
+ctx.lineWidth= 3;
+
+var circum = 3 * Math.PI / 2;
+var dPos = (Math.PI * 4) / 600;
+
+let fps = 60;
+var startTime = Date.now();
+let frameDuration = 1000/fps;
+var loopStart = 0;
+
+draw = function() {
+    requestAnimationFrame(draw);
+    ctx.clearRect(0, 0, innerWidth, innerHeight);
+
+    ctx.beginPath();
+    ctx.arc(innerWidth / 4, 768 / 1.7, 375, circum, 3 * Math.PI / 2);
+    ctx.stroke();
+
+    circum += dPos;
+
+    
+}
+function circle(circum){
+    ctx.beginPath();
+    ctx.arc(innerWidth / 4, 768 / 1.7, 375, circum, 3 * Math.PI / 2);
+    ctx.stroke();
+}
+
+function mainLoop(){
+    requestAnimationFrame(mainLoop);
+    startTime = Date.now();
+    if (startTime >= loopStart){
+        update();
+        circle(circum);
+        loopStart = startTime + frameDuration;
+
+    }
+}
+
+function update(){
+    ctx.clearRect(0, 0, innerWidth, innerHeight);
+    circum += dPos;
+}
+
+mainLoop();
