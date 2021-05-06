33550d0f0e2daeadc911efa0f6ad82ecfe439c5a
Dar Vince Ocarol <55413710+OcarolDV@users.noreply.github.com>
2021-05-05 20:39:21 -0700

    Styling + BG update

--- script.js
+++ script.js
@@ -4,45 +4,61 @@ canvas.height = 650;
 
 
 let ctx = canvas.getContext("2d");
+
+let note1 = document.getElementById("note");
+
 ctx.fillStyle = '#ed4242';
 ctx.strokeStyle = '#ed4242';
 ctx.lineWidth= 3;
 
 var circum = 3 * Math.PI / 2;
-var dPos = (Math.PI * 4) / 600;
 
 let fps = 60;
 var startTime = Date.now();
 let frameDuration = 1000/fps;
+
 var loopStart = 0;
+var timeStart = 120 * 1000;
+var dPos = ((Math.PI * 2) / (timeStart * 3/4) * frameDuration);
+var minutes;
+var seconds;
 
-draw = function() {
-    requestAnimationFrame(draw);
-    ctx.clearRect(0, 0, innerWidth, innerHeight);
+// function draw() {
+//     requestAnimationFrame();
+//     ctx.clearRect(0, 0, innerWidth, innerHeight);
+//     time1.innerHTML = timestamp;
+//     console.log(time1);
+//     // ctx.beginPath();
+//     // ctx.arc(canvas.width / 2, canvas.height, 375, circum, 3 * Math.PI / 2);
+//     // ctx.stroke();
+    
 
-    ctx.beginPath();
-    ctx.arc(canvas.width / 2, canvas.height, 375, circum, 3 * Math.PI / 2);
-    ctx.stroke();
-
-    circum += dPos;
 
     
-}
-function circle(circum){
+// }
+function circle(){
     ctx.beginPath();
     ctx.arc(canvas.width / 2, canvas.height / 2, 315, circum, 3 * Math.PI / 2);
     ctx.stroke();
 }
 
-function mainLoop(){
+function mainLoop(timestamp){
     requestAnimationFrame(mainLoop);
-    startTime = Date.now();
+    startTime = timestamp;
     if (startTime >= loopStart){
         update();
-        circle(circum);
-        loopStart = startTime + frameDuration;
+        
+        circle();
+        minutes = Math.floor(((timeStart - Math.round(timestamp)) % (1000 * 60 * 60)) / (1000 * 60));
+        seconds = Math.floor(((timeStart - Math.round(timestamp)) % (1000 * 60)) / 1000);
+        note1.innerHTML =  minutes + "m " + seconds + "s ";
+        if (timestamp > timeStart){
+            note1.innerHTML =  "Relax.."
+        }
+        loopStart = timestamp + frameDuration;
 
     }
+
 }
 
 function update(){
